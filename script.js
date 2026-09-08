9564e3b238e055dd5e3bfe89d3e9f1ff87096005
glucoseP <sakku3564@gmail.com>
2026-09-09 01:13:19 +0900

    Update script.js

--- script.js
+++ script.js
@@ -23,3 +23,28 @@ navItems.forEach((item, index) => {
     });
 
 });
+
+const registerButton = document.getElementById("register-button");
+
+registerButton.addEventListener("click", () => {
+
+    const name = document.getElementById("organism-name").value;
+    const place = document.getElementById("organism-place").value;
+    const date = document.getElementById("organism-date").value;
+    const note = document.getElementById("organism-note").value;
+
+    if (!name) {
+        alert("生き物の名前を入力してね！");
+        return;
+    }
+
+    console.log({
+        name: name,
+        place: place,
+        date: date,
+        note: note
+    });
+
+    alert(`${name}を図鑑に登録したよ！`);
+
+});
