6ff765428e0b30dc4f664573c63077ecc9c45a19
glucoseP <sakku3564@gmail.com>
2026-09-09 01:07:45 +0900

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,25 @@
+const navItems = document.querySelectorAll(".nav-item");
+const pages = document.querySelectorAll(".page");
 
+navItems.forEach((item, index) => {
+
+    item.addEventListener("click", () => {
+
+        // ナビの選択状態を変更
+        navItems.forEach(nav => {
+            nav.classList.remove("active");
+        });
+
+        item.classList.add("active");
+
+
+        // ページを切り替える
+        pages.forEach(page => {
+            page.classList.remove("active-page");
+        });
+
+        pages[index].classList.add("active-page");
+
+    });
+
+});
